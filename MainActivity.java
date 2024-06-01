c74a487f7d1990091cbc5c2c237b0f25a8155456
palak878 <161827204+palak878@users.noreply.github.com>
2024-06-01 08:29:00 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,80 +1,71 @@
-package com.example.prc_1;
+package com.example.messageapk;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
-    EditText et1,et2;
-    Button add,sub,div,mul;
-    TextView ans;
-@Override
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtMobile;
+    private EditText txtMessage;
+    private Button btnSms;
+
+    private static final int PERMISSION_REQUEST_SEND_SMS = 1;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = findViewById(R.id.et1);
-        et2 = findViewById(R.id.et2);
-        add = findViewById(R.id.add);
-        sub = findViewById(R.id.sub);
-        div= findViewById(R.id.div);
-        mul = findViewById(R.id.mul);
-        ans = findViewById(R.id.ans);
+        txtMobile = findViewById(R.id.mblTxt);
+        txtMessage = findViewById(R.id.msgTxt);
+        btnSms = findViewById(R.id.btnSend);
 
-    add.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int firstvalue, secondvalue, answer;
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissionAndSendSms();
+            }
+        });
+    }
 
-            firstvalue = Integer.parseInt(et1.getText().toString());
-            secondvalue = Integer.parseInt(et2.getText().toString());
-            answer = firstvalue + secondvalue;
-            ans.setText(String.valueOf(answer));
+    private void checkPermissionAndSendSms() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_SEND_SMS);
+        } else {
+            sendSms();
         }
-    });
+    }
 
-    mul.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int firstvalue, secondvalue, answer;
-
-            firstvalue = Integer.parseInt(et1.getText().toString());
-            secondvalue = Integer.parseInt(et2.getText().toString());
-            answer = firstvalue * secondvalue;
-            ans.setText(String.valueOf(answer));
+    private void sendSms() {-
+        try {
+            SmsManager smgr = SmsManager.getDefault();
+            smgr.sendTextMessage(txtMobile.getText().toString(), null, txtMessage.getText().toString(), null, null);
+            Toast.makeText(MainActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
         }
-    });
+    }
 
-    sub.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int firstvalue, secondvalue, answer;
-
-            firstvalue = Integer.parseInt(et1.getText().toString());
-            secondvalue = Integer.parseInt(et2.getText().toString());
-            answer = firstvalue - secondvalue;
-            ans.setText(String.valueOf(answer));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_SEND_SMS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendSms();
+            } else {
+                Toast.makeText(MainActivity.this, "Permission denied, cannot send SMS", Toast.LENGTH_SHORT).show();
+            }
         }
-    });
-
-    div.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int firstvalue, secondvalue, answer;
-
-            firstvalue = Integer.parseInt(et1.getText().toString());
-            secondvalue = Integer.parseInt(et2.getText().toString());
-            answer = firstvalue / secondvalue;
-            ans.setText(String.valueOf(answer));
-        }
-    });
-
-
-
-
-}
+    }
 }
